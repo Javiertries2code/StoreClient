ba771138002ec8928973b70a9f6821c6de40c8e4
Javiertries2code <superjabo@live.com>
2025-05-08 20:06:58 +0200

    improved the Appfragment enum, using lambda that return a new instance... for the shake of correctness :-)

--- app/src/main/java/com/example/storeclient/enums/AppFragments.kt
+++ app/src/main/java/com/example/storeclient/enums/AppFragments.kt
@@ -1,39 +1,26 @@
 package com.example.storeclient.enums
 
-
-
 import androidx.fragment.app.Fragment
 import com.elorrieta.alumnoclient.LandingFragment
-import com.example.storeclient.ui.fragments.Add_Product_Fragment
-import com.example.storeclient.ui.fragments.AdminFragment
-import com.example.storeclient.ui.fragments.AdminUsersFragment
-import com.example.storeclient.ui.fragments.DispatchNoteFragment
-import com.example.storeclient.ui.fragments.InventoryFragment
-import com.example.storeclient.ui.fragments.ProdutcsFragment
-import com.example.storeclient.ui.fragments.PublicProductsFragment
-import com.example.storeclient.ui.fragments.TestFragment
-import com.example.storeclient.ui.fragments.UsersFragment
+import com.example.storeclient.ui.fragments.*
 import com.example.storeclient.ui.login.LoginFragment
 
 enum class AppFragments(
-    val fragmentClass: Class<out Fragment>,
-    val isProtected: Boolean = true // default to protected
+    val fragmentFactory: () -> Fragment,
+    val isProtected: Boolean = true
 ) {
-    TEST_FRAGMENT(TestFragment::class.java, false),
-    DEV_FRAGMENT(ProdutcsFragment::class.java, true),
-    LOGIN_FRAGMENT(LoginFragment::class.java, false),
-    LANDING_FRAGMENT(LandingFragment::class.java, false),
-    USERS_FRAGMENT(UsersFragment::class.java, false),
-    PRODUCTS_FRAGMENT(ProdutcsFragment::class.java, false),
-    ADMIN_FRAGMENT(AdminFragment::class.java),
-    ADMIN_USERS_FRAGMENT(AdminUsersFragment::class.java),
-    DISPATCH_NOTE_FRAGMENT(DispatchNoteFragment::class.java),
-    INVENTORY_FRAGMENT(InventoryFragment::class.java),
-    ADD_PRODUCT_FRAGMENT(Add_Product_Fragment::class.java, false),
-    PUBLIC_PRODUCTS_FRAGMENT(PublicProductsFragment::class.java, true);
+    TEST_FRAGMENT({ TestFragment() }, false),
+    DEV_FRAGMENT({ ProdutcsFragment() }, true),
+    LOGIN_FRAGMENT(::LoginFragment, false),
+    LANDING_FRAGMENT({ LandingFragment() }, false),
+    USERS_FRAGMENT({ UsersFragment() }, false),
+    PRODUCTS_FRAGMENT({ ProdutcsFragment() }, false),
+    ADMIN_FRAGMENT({ AdminFragment() }),
+    ADMIN_USERS_FRAGMENT({ AdminUsersFragment() }),
+    DISPATCH_NOTE_FRAGMENT({ DispatchNoteFragment() }),
+    INVENTORY_FRAGMENT({ InventoryFragment() }),
+    ADD_PRODUCT_FRAGMENT({ Add_Product_Fragment() }, false),
+    PUBLIC_PRODUCTS_FRAGMENT({ PublicProductsFragment() }, true);
 
-
-    fun newInstance(): Fragment {
-        return fragmentClass.newInstance()
-    }
+    fun newInstance(): Fragment = fragmentFactory()
 }
